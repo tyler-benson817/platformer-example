eb0614cb364d87aff08641eba04ac3a690459f7c
tyler-benson817 <tylerjames200869@gmail.com>
2025-10-23 12:03:26 +0100

    more stuff

--- 2D_Platformer/Assets/Scenes/scripts/pllayermovementscript.cs
+++ 2D_Platformer/Assets/Scenes/scripts/pllayermovementscript.cs
@@ -8,10 +8,12 @@ public class pllayermovementscript : MonoBehaviour
     public float m_Speed = 10.0f; 
     Rigidbody2D m_Rigidbody;
     public float m_JumpPower = 10.0f;
-    // start is called before the first frame update
+    public int m_MaxJumps = 2;
+    private int m_currentjumps;
 
     void Start()
     {
+        m_currentjumps = m_MaxJumps;
         m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -24,9 +26,16 @@ public class pllayermovementscript : MonoBehaviour
 
         m_Rigidbody.linearVelocity = new Vector2(xMove * m_Speed, m_Rigidbody.linearVelocity.y);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) && m_currentjumps>0)
         {
             m_Rigidbody.AddForce(Vector2.up * m_JumpPower, ForceMode2D.Impulse);
+            m_currentjumps--;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+            m_currentjumps = m_MaxJumps;
     }
 }
